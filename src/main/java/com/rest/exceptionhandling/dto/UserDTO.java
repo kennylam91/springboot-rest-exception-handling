198339d55af8698af3803ad02cdd10a3ef1be22d
Pham Van Lam <phamvanlam1991@gmail.com>
2020-02-06 10:50:59 +0700

    create api + dto + test success api: return json object + 201 created

---
+++ src/main/java/com/rest/exceptionhandling/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package com.rest.exceptionhandling.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO implements Serializable{
+
+	 
+	private static final long serialVersionUID = -8949736803172662358L;
+
+	private String username;
+	
+	private String password;
+
+	public UserDTO(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
