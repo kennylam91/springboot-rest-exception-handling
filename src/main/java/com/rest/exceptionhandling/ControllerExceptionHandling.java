be289e52aa9062324bc83409554a8adf2f677fda
Pham Van Lam <phamvanlam1991@gmail.com>
2020-02-08 09:23:51 +0700

    spring-web-problem did work

---
+++ src/main/java/com/rest/exceptionhandling/ControllerExceptionHandling.java
@@ -0,0 +1,10 @@
+package com.rest.exceptionhandling;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+
+@ControllerAdvice
+public class ControllerExceptionHandling implements ProblemHandling{
+	
+
+}
--- src/main/java/com/rest/exceptionhandling/ExceptionHandlingApplication.java
+++ src/main/java/com/rest/exceptionhandling/ExceptionHandlingApplication.java
@@ -2,6 +2,11 @@ package com.rest.exceptionhandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.validation.ConstraintViolationProblemModule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class ExceptionHandlingApplication {
@@ -9,5 +14,9 @@ public class ExceptionHandlingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionHandlingApplication.class, args);
 	}
-
+	/*
+	 * @Bean public ObjectMapper objectMapper() { return new
+	 * ObjectMapper().registerModules( new ProblemModule(), new
+	 * ConstraintViolationProblemModule()); }
+	 */
 }
